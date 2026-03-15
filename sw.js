030726c1a837c3388a01ad7b3a94c89aa43c7e2d
sukmaajidigital <sukmaaji.digital@gmail.com>
2026-03-16 01:52:59 +0700

    add icons, screenshots, and service worker for PWA support

---
+++ sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = "jisclock-cache-v2";
+const CORE_ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest?v=2",
+  "./icons/icon-192.svg",
+  "./icons/icon-512.svg",
+  "./screenshots/clock-wide.svg",
+  "./screenshots/clock-narrow.svg",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))),
+      ),
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request)
+        .then((networkResponse) => {
+          const responseClone = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseClone);
+          });
+          return networkResponse;
+        })
+        .catch(() => caches.match("./index.html"));
+    }),
+  );
+});
